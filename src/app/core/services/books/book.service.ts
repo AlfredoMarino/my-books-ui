8273db0963710d8c797170c4b3ef325d28ef3cf1
Alfredo Marino <alfredoalejandro05@gmail.com>
2020-09-18 14:59:21 -0300

    environment init config 3

--- src/app/core/services/books/book.service.ts
+++ src/app/core/services/books/book.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Book} from '@core/models/book.model';
-import {retry} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Book } from '@core/models/book.model';
+import { retry } from 'rxjs/operators';
+import { environment } from "@environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-
-  private HOST = 'http://192.168.99.100:5000/v1';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,18 +17,18 @@ export class BookService {
   constructor(private http: HttpClient) { }
 
   getBooksByPersonId(personId: number): Observable<Book[]> {
-    return this.http.get<Book[]>(`${(this.HOST)}/persons/${personId}/libraries/books`);
+    return this.http.get<Book[]>(`${environment.myBooksServiceApiURL}/persons/${personId}/libraries/books`);
   }
 
   getBook(bookId: string) {
-    return this.http.get<Book>(`${(this.HOST)}/books/${bookId}`)
+    return this.http.get<Book>(`${environment.myBooksServiceApiURL}/books/${bookId}`)
       .pipe(
         retry(1)
       );
   }
 
   getBooksByName(name: string): Observable<Book[]>{
-    return this.http.get<Book[]>(`${(this.HOST)}/books?name=${name}`);
+    return this.http.get<Book[]>(`${environment.myBooksServiceApiURL}/books?name=${name}`);
   }
 
 }
