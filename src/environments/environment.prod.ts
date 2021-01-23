eef7c5ab940ef5b6690e3ea80099efb187f15611
Alfredo Marino <alfredoalejandro05@gmail.com>
2021-01-22 22:42:43 -0300

    api url port prod changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  myBooksServiceApiURL: 'http://localhost:5000/my-books-service/api/v1',
+  myBooksServiceApiURL: 'http://localhost:31000/my-books-service/api/v1',
   personId: 20
 };
