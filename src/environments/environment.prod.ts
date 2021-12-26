c6df1cd5d6c51744566e5db10fa6fc105c454073
Alfredo Marino <alfredoalejandro05@gmail.com>
2021-12-26 01:05:01 +0100

    prod my-book-service url port to 5000

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  myBooksServiceApiURL: 'http://localhost:31000/my-books-service/api/v1',
+  // myBooksServiceApiURL: 'http://localhost:31000/my-books-service/api/v1',
+  myBooksServiceApiURL: 'http://localhost:5000/my-books-service/api/v1',
   personId: 20,
   countryId: 95,
   regionId: 1886
